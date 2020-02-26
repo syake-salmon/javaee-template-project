9c3d30da23431d41958eb836477b232abe018645
syakesalmon <syakeApps@icloud.com>
2020-02-26 20:18:11 +0900

    refactoring

--- src/main/java/com/syakeapps/jtp/logging/LoggerFactory.java
+++ src/main/java/com/syakeapps/jtp/logging/LoggerFactory.java
@@ -8,13 +8,28 @@ import org.slf4j.cal10n.LocLoggerFactory;
 import ch.qos.cal10n.IMessageConveyor;
 import ch.qos.cal10n.MessageConveyor;
 
+/**
+ * {@link LocLogger} provider.
+ */
 public class LoggerFactory {
 
-    public static LocLogger getLogger(Class<?> clazz) {
+    private LoggerFactory() {
+        throw new IllegalStateException("Unsupported feature.");
+    }
+
+    /**
+     * Get a new {@link LocLogger} instance by class. The returned
+     * {@link LocLogger} will be named after the class. The {@link LocLogger}
+     * will be localized by {@link Locale#getDefault()}.
+     *
+     * @param clazz
+     * @return {@link LocLogger} instance by class
+     */
+    public static LocLogger getLogger(final Class<?> clazz) {
         return getLogger(clazz, Locale.getDefault());
     }
 
-    private static LocLogger getLogger(Class<?> clazz, Locale loc) {
+    private static LocLogger getLogger(final Class<?> clazz, final Locale loc) {
         IMessageConveyor conveyor = new MessageConveyor(loc);
         return new LocLoggerFactory(conveyor).getLocLogger(clazz);
     }
--- src/main/java/com/syakeapps/jtp/logging/Messages.java
+++ src/main/java/com/syakeapps/jtp/logging/Messages.java
@@ -4,6 +4,9 @@ import ch.qos.cal10n.BaseName;
 import ch.qos.cal10n.Locale;
 import ch.qos.cal10n.LocaleData;
 
+/**
+ * Enum of logging messages.
+ */
 @BaseName("messages")
 @LocaleData(defaultCharset = "UTF8",
         value = { @Locale("en_US"), @Locale("ja_JP") })
