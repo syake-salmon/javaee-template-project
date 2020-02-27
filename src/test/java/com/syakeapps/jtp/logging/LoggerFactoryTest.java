83d7e5b6e83c184f702e5de3e8d2dfeac4f6302f
syakesalmon <syakeApps@icloud.com>
2020-02-27 12:10:01 +0900

    delete needless sources

--- src/test/java/com/syakeapps/jtp/logging/LoggerFactoryTest.java
+++ src/test/java/com/syakeapps/jtp/logging/LoggerFactoryTest.java
@@ -1,65 +1,33 @@
 package com.syakeapps.jtp.logging;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotNull;
 
-import java.util.List;
 import java.util.Locale;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.slf4j.cal10n.LocLogger;
 
-import com.syakeapps.jtp.testutil.TestLogger;
+import ch.qos.cal10n.IMessageConveyor;
 
-@RunWith(PowerMockRunner.class)
-@PrepareForTest({ org.slf4j.LoggerFactory.class })
 public class LoggerFactoryTest {
 
-    private TestLogger logger;
-
-    @Before
-    public void doBefore() throws Exception {
-        logger = new TestLogger();
-
-        PowerMockito.mockStatic(org.slf4j.LoggerFactory.class);
-        PowerMockito
-                .when(org.slf4j.LoggerFactory
-                        .getLogger(LoggerFactoryTest.class.getName()))
-                .thenReturn(logger);
-    }
-
+    // Test getting LocLogger that is localized by default locale.
     @Test
-    public void test_getLogger_ja() throws Exception {
+    public void test_getLogger() throws Exception {
         /* SETUP */
-        Locale.setDefault(Locale.JAPAN);
+        Locale expected = Locale.getDefault();
 
         /* INVOCATION */
-        LocLogger locLogger = LoggerFactory.getLogger(LoggerFactoryTest.class);
-        locLogger.info(Messages.EXAMPLE);
-        List<String> actual = logger.getEvents();
+        LocLogger logger = LoggerFactory.getLogger(LoggerFactoryTest.class);
+        IMessageConveyor imc = (IMessageConveyor) PowerMockito
+                .field(logger.getClass(), "imc").get(logger);
+        Locale actual = (Locale) PowerMockito.field(imc.getClass(), "locale")
+                .get(imc);
 
         /* ASSERTION */
-        assertTrue(actual.size() == 1);
-        assertEquals("これはサンプルです。", actual.get(0));
-    }
-
-    @Test
-    public void test_getLogger_en() throws Exception {
-        /* SETUP */
-        Locale.setDefault(Locale.US);
-
-        /* INVOCATION */
-        LocLogger locLogger = LoggerFactory.getLogger(LoggerFactoryTest.class);
-        locLogger.info(Messages.EXAMPLE);
-        List<String> actual = logger.getEvents();
-
-        /* ASSERTION */
-        assertTrue(actual.size() == 1);
-        assertEquals("This is a example.", actual.get(0));
+        assertNotNull(logger);
+        assertEquals(expected, actual);
     }
 }
